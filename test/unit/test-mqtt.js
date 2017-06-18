653d2907973cd0f72ac59fdf399ef5f14f22b1ba
heartyoh <heartyoh@gmail.com>
2017-06-18 17:13:18 +0900

    initial import

---
+++ test/unit/test-mqtt.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2017 HatioLab Inc. All rights reserved.  
+ */
+
+import './util'
+
+import { expect } from 'chai'
+
+import '../../bower_components/things-scene-core/things-scene-min'
+import { Mqtt } from '../../src/index'
+
+describe('Mqtt', function () {
+
+  var board;
+
+  beforeEach(function () {
+    board = scene.create({
+      model: {
+        components: [{
+          id: 'mqtt',
+          type: 'mqtt'
+        }]
+      }
+    })
+  });
+
+  it('component should be found by its id.', function () {
+
+    var component = board.findById('mqtt')
+
+    expect(!!component).not.to.equal(false);
+  });
+});
